200a365735a2e2369f1b191f8e4c92d5701a7f56
Dylan Lisk <53537700+Dlisk92@users.noreply.github.com>
2020-06-17 19:53:07 -0700

    Add files via upload

--- get_history.py
+++ get_history.py
@@ -24,25 +24,28 @@ accountid = response.json()['accounts'][0]['id']
 
 
 ## Use this to get the price history
-periods = 5
+periods = 5000
 params = (
     ('count', f'{periods}'),
     ('price', 'M'),
     ('granularity', 'M1'),
 )
-hist = {}
+
 for symbol in SYMBOLS:
     candle = requests.get(f'https://api-fxpractice.oanda.com/v3/instruments/{symbol}/candles', headers=headers, params=params)
-    
+    hist = pd.DataFrame()
     for i in range(0,periods):
-            print(candle.json()['candles'][i])
-            hist[symbol+str(i)] = candle.json()['candles'][i]
-            
+        # print(pd.DataFrame(candle.json()['candles'][i]))
+            # hist[symbol+str(i)] = candle.json()['candles'][i]['mid']['c']
+            # hist[symbol+'time'+str(i)] = candle.json()['candles'][i]['time']
+        hist = hist.append(pd.DataFrame(candle.json()['candles'][i]), ignore_index=True)
+    hist.to_json(f'{symbol}.json')
 
-with open("history.json", 'w') as f:
-    # indent=2 is not needed but makes the file 
-    # human-readable for more complicated data
-    json.dump(hist, f,) 
+
+# with open("history.json", 'w') as f:
+#     # indent=2 is not needed but makes the file 
+#     # human-readable for more complicated data
+#     json.dump(hist, f,) 
 
 
 
